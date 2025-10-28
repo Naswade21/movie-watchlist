a8a541bf850292f52a9b1a7608085902d5c36c34
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2025-10-28 14:29:23 -0400

    Working on read more for movie descriptions

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+//work on limiting lines in para and work on css
+
 const searchForm = document.getElementById('search-form')
 const searchInput = document.getElementById('search')
 const watchContent = document.getElementById('watch-content')
 const movieContent = document.getElementById('movie-content')
 export let watchArr = []
+
 
 document.addEventListener('click', (e) => {
     if(e.target.dataset.movie){
@@ -46,17 +49,17 @@ const getMovieHtml = (arr) => {
             <div class="movie-content">
                 <div class="movie-title-wrap">
                     <div>${movie.Title}</div>
-                    <i class="fa-solid fa-star"></i>
+                    <div><i class="fa-solid fa-star"></i></div>
                     <div>${movie.imdbRating}</div>
                 </div>
                 <div class="movie-detail-wrap">
                     <div>${movie.Runtime}</div>
                     <div>${movie.Genre}</div>
                     <div>
-                    <a data-movie="${movie.imdbID}"><i class="fa-solid fa-circle-plus"></i>Watchlist</a>
+                    <a data-movie="${movie.imdbID}"><i class="fa-solid fa-circle-plus"></i> Watchlist</a>
                     </div>
                 </div>
-                <p>${movie.Plot}</p>
+                <p data-text="${movie.imdbID}">${movie.Plot}</p>
             </div>
         </div>
         `
@@ -67,80 +70,4 @@ const renderMovies = (arr) => {
     return movieContent.innerHTML = getMovieHtml(arr)
 }
 
-/*
-Actors
-: 
-"Chris Evans, Samuel L. Jackson, Scarlett Johansson"
-Awards
-: 
-"Nominated for 1 Oscar. 5 wins & 52 nominations total"
-BoxOffice
-: 
-"$259,766,572"
-Country
-: 
-"United States"
-DVD
-: 
-"N/A"
-Director
-: 
-"Anthony Russo, Joe Russo"
-Genre
-: 
-"Action, Adventure, Sci-Fi"
-Language
-: 
-"English, French"
-Metascore
-: 
-"70"
-Plot
-: 
-"As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier."
-Poster
-: 
-"https://m.media-amazon.com/images/M/MV5BNWY1NjFmNDItZDhmOC00NjI1LWE0ZDItMTM0MjBjZThiOTQ2XkEyXkFqcGc@._V1_SX300.jpg"
-Production
-: 
-"N/A"
-Rated
-: 
-"PG-13"
-Ratings
-: 
-(3) [{…}, {…}, {…}]
-Released
-: 
-"04 Apr 2014"
-Response
-: 
-"True"
-Runtime
-: 
-"136 min"
-Title
-: 
-"Captain America: The Winter Soldier"
-Type
-: 
-"movie"
-Website
-: 
-"N/A"
-Writer
-: 
-"Christopher Markus, Stephen McFeely, Joe Simon"
-Year
-: 
-"2014"
-imdbID
-: 
-"tt1843866"
-imdbRating
-: 
-"7.7"
-imdbVotes
-: 
-"951,315"
-*/
+
